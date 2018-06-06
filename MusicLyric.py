1f687cfd0c4d8a05537d8449d7be26baccd160ea
SaturnL <171098574@smail.nju.edu.cn>
2018-06-06 21:48:27 +0800

    0605

--- MusicLyric.py
+++ MusicLyric.py
@@ -5,8 +5,13 @@
 import requests
 from bs4 import BeautifulSoup
 import general
+import json
 
-url='http://music.163.com/song?id=557581284'
+url='557581284'
+lyric_url=general.lyric_url.format(url)
+print(lyric_url)
 k=requests.session()
-k=BeautifulSoup(k.get(url,headers=general.headers).content,"lxml")
-print(k)
+k=BeautifulSoup(k.get(lyric_url,headers=general.headers).content,'lxml')
+k = json.loads(k.find('p').text)
+lyric=k['lrc']['lyric']
+lyric.replace('')
